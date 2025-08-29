d463107b8d2aca5d5b0070056b88329e11ce6e54
Bhavitha M <b44068845@gmail.com>
2025-08-29 11:27:28 +0530

    update the jar file

--- springboot-webhook/springboot-webhook/src/main/java/com/example/springboot_webhook/service/StartupService.java
+++ springboot-webhook/springboot-webhook/src/main/java/com/example/springboot_webhook/service/StartupService.java
@@ -68,7 +68,7 @@ public class StartupService implements CommandLineRunner {
         String url = "https://bfhldevapigw.healthrx.co.in/hiring/testWebhook/JAVA";
 
         // ⚡ Replace this with your actual final SQL query
-        String finalQuery = "SELECT * FROM employee;";
+        String finalQuery = "SELECT e1.EMP_ID, e1.FIRST_NAME, e1.LAST_NAME, d.DEPARTMENT_NAME, COUNT(e2.EMP_ID) AS YOUNGER_EMPLOYEES_COUNT FROM EMPLOYEE e1 JOIN DEPARTMENT d ON e1.DEPARTMENT_ID = d.DEPARTMENT_ID LEFT JOIN EMPLOYEE e2 ON e1.DEPARTMENT_ID = e2.DEPARTMENT_ID AND e2.DOB > e1.DOB GROUP BY e1.EMP_ID, e1.FIRST_NAME, e1.LAST_NAME, d.DEPARTMENT_NAME ORDER BY e1.EMP_ID DESC;";
 
         RestTemplate restTemplate = new RestTemplate();
 
